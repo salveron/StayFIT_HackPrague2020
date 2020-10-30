ac25b18ecf10e2a22c6bed2d1304b558f10166d0
Daniil Poletaev <danpoletaev1@gmail.com>
2020-10-31 00:49:58 +0100

    Added phone verification

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-key"
     DEBUG = True
     CSRF_ENABLED = True
+    AUTHY_API_KEY = 'nTzk00XPD02V1v3hevVRTc7PQzkiHk0C'
 
 
 class ConfigDB(Config):
